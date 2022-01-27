a1c2496cebab062f5359a7f4aedafbe935576f9f
amatshiye <amatshiy@student.42.fr>
2022-01-28 01:30:48 +0200

    added pause, resume in the player class

--- src/structures/player.ts
+++ src/structures/player.ts
@@ -3,6 +3,7 @@ import DisTube, {
   DisTubeOptions,
   DisTubeVoiceManager,
   GuildIdResolvable,
+  Queue,
 } from "distube";
 import { client } from "..";
 import { Player } from "../typings/player";
@@ -37,6 +38,16 @@ class ExtendedPlayer implements Player {
     let channel: VoiceBasedChannel = member.voice.channel as VoiceBasedChannel;
 
     await this.distube.play(channel, query);
+  }
+
+  pause(guild: GuildIdResolvable): void {
+    const queue: Queue = this.distube.getQueue(guild) as Queue;
+    queue?.pause();
+  }
+
+  resume(guild: GuildIdResolvable): void {
+    const queue: Queue = this.distube.getQueue(guild) as Queue;
+    queue.resume();
   }
 }
 
